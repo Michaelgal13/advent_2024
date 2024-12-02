09b6f8c4a1594ad5bd59c6302877b2951ed9686d
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2024-12-01 21:01:29 -0800

    Finished advent day 1

--- day_1/advent_day_1.py
+++ day_1/advent_day_1.py
@@ -6,8 +6,10 @@ from typing import List
 
 #Test lists
 #Difference between list 0 and 1 = 4 + 3 + 2 = 9
+#Similarity between list 0 and 2 = 7 + 10 = 17
 test_list_0 = [7, 5, 3]
 test_list_1 = [-1, 9, 2]
+test_list_2 = [7, 5, 5]
 
 def list_distances(list_0: List[int], list_1: List[int], debug:int=None):
     if(debug):
@@ -28,13 +30,33 @@ def list_distances(list_0: List[int], list_1: List[int], debug:int=None):
         manhattan_distance = manhattan_distance + (abs(list_0[count] - list_1[count]))
     return manhattan_distance
 
-
+def list_similarity(list_0: List[int], list_1: List[int], debug:int=None):
+    #Note the challenge does not talk about negative numbers
+    data_dict = dict()
+    for items in list_1:
+        #Would have to check if dict serach is faster than sorting each time
+        #Create new key if it does not exist, otherwise increment
+        if items in data_dict.keys():
+            data_dict[items] = data_dict[items] + 1
+        else:
+            data_dict[items] = 1
+    if(debug):
+        print("Temp Dictionary")
+        print(data_dict)
+    similarity_score = 0
+    for items in list_0:
+        if items in data_dict.keys():
+            similarity_score = similarity_score + (items * data_dict[items])
+    return similarity_score
 
 
 def main():
     #Python doesn't have an argc :(
     if(len(sys.argv) <= 1):
+        print("List Distances:")
         print(list_distances(test_list_0, test_list_1, None))
+        print("List Similarity:")
+        print(list_similarity(test_list_0, test_list_2, None))
     else:
         # print(str(sys.argv[1]))
         if(os.path.isfile(sys.argv[1])):
@@ -48,8 +70,10 @@ def main():
         else:
             exception_str = "Could not find file: " + str(sys.argv[1])
             raise Exception(exception_str)
+        print("List Distances:")
         print(list_distances(list_0, list_1, None))
-        #Assuming user input a good string, big assumption
+        print("List Similarity:")
+        print(list_similarity(list_0, list_1, None))
 
 if __name__ == "__main__":
     main()
